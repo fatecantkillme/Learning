8e33f3ef50f345f6698ed17080693f34bb027b85
姚骞 <145198570+fatecantkillme@users.noreply.github.com>
2024-10-19 03:39:52 +0000

    ceshi

--- simple_switch_13.py
+++ simple_switch_13.py
@@ -86,104 +86,84 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+        dpid = datapath.id
+
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-        
-        # 忽略 LLDP 包
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
             return
         
-        src = eth.src
-        dst = eth.dst
-        dpid = datapath.id
         
-        # 获取源IP和目的IP
-        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
-        if ipv4_pkt:
-            src_ip = ipv4_pkt.src
-            dst_ip = ipv4_pkt.dst
-            
-            # 查找源主机和目标主机对应的DPID
-            src_dpid = self.hosts.get(src_ip)
-            dst_dpid = self.hosts.get(dst_ip)
-            
-            if src_dpid and dst_dpid:
-                # 使用DFS查找路径
-                paths = self.DFS(self.topology, src_dpid, dst_dpid)
-                if paths:
-                    # 找到最短路径和最长路径
-                    shortest_path = min(paths, key=len)
-                    longest_path = max(paths, key=len)
-                    self.logger.info("Shortest Path: %s", shortest_path)
-                    self.logger.info("Longest Path: %s", longest_path)
-                    
-                    # 使用最长路径下发流表规则
-                    self.install_path(longest_path, src_ip, dst_ip, datapath)
-                    return  # 下发后可以直接返回，避免重复处理
 
-        # 如果没有找到路径或者没有IP地址关联时，使用默认的FLOOD处理
+        dst = eth.dst
+        src = eth.src
+
+        dpid = format(datapath.id, "d").zfill(16)
+
         self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
+
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
+
         actions = [parser.OFPActionOutput(out_port)]
-        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                in_port=in_port, actions=actions, data=data)
+                                  in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
-
-
-    def install_path(self, path, src_ip, dst_ip, datapath):
-        parser = datapath.ofproto_parser
-        for i in range(len(path) - 1):
-            dpid = path[i]
-            next_dpid = path[i + 1]
-            out_port = self.get_port(dpid, next_dpid)  # 获取端口信息
-            match = parser.OFPMatch(eth_type=0x0800, ipv4_src=src_ip, ipv4_dst=dst_ip)
-            actions = [parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, 1, match, actions)
-
-
     @set_ev_cls(event.EventSwitchEnter)
     def get_topology(self, ev):
         time.sleep(1)
-        
-        # 获取交换机和链路信息
         switches = get_switch(self, None)
-        switch_list = [switch.dp.id for switch in switches]
+        switch_list=[switch.dp.id for switch in switches]
         links = get_link(self, None)
         
-        # 打印交换机和链路信息
         self.logger.info("Switches: %s", switch_list)
         self.logger.info("Links: %s", links)
-        
-        # 遍历链路并建立拓扑
+
+        # Iterate over each link and only add unique connections
         for link in links:
             src = link.src.dpid
             dst = link.dst.dpid
-            
+
+            # Ensure that the link isn't already present before adding
             if dst not in self.topology.get(src, []):
                 self.topology.setdefault(src, []).append(dst)
             if src not in self.topology.get(dst, []):
                 self.topology.setdefault(dst, []).append(src)
-        
+
         self.logger.info("Topology: %s", self.topology)
-        
-        # 获取主机信息并建立 IP 与 DPID 的关联
-        hosts = get_all_host(self, None)
-        for host in hosts:
-            if host.ipv4:  # 检查主机是否有 IPv4 地址
-                self.hosts[host.ipv4[0]] = host.port.dpid  # 关联主机的IP与其连接的交换机DPID
-                self.logger.info("Host: %s, DPID: %s", host.ipv4[0], host.port.dpid)
-
-
 
     
     def DFS(self,graph, src, dst, path=None):
