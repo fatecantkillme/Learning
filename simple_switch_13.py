6fb962958618e9936d5686f1029ad82ad0678b1f
姚骞 <145198570+fatecantkillme@users.noreply.github.com>
2024-10-20 00:59:07 +0000

    pl2

--- simple_switch_13.py
+++ simple_switch_13.py
@@ -51,55 +51,61 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        # 如果不是 ARP 数据包，直接忽略
-        if eth.ethertype != ether_types.ETH_TYPE_ARP:
-            self.logger.info("Non-ARP packet received, ethertype: %s", eth.ethertype)
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
             return
+        dst = eth.dst
+        src = eth.src
 
-        # 处理 ARP 数据包
-        arp_pkt = pkt.get_protocol(arp.arp)
-        if arp_pkt:
-            src_mac = arp_pkt.src_mac
-            src_ip = arp_pkt.src_ip
-            dst_ip = arp_pkt.dst_ip
+        dpid = format(datapath.id, "d").zfill(16)
+        self.mac_to_port.setdefault(dpid, {})
 
-            self.logger.info("ARP packet received: src_mac=%s, src_ip=%s, dst_ip=%s", src_mac, src_ip, dst_ip)
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-            # 更新 MAC 到端口的映射
-            dpid = datapath.id
-            self.mac_to_port.setdefault(dpid, {})
-            self.mac_to_port[dpid][src_mac] = in_port
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
 
-            # 查找目标 IP 对应的 MAC 地址，如果知道目的 MAC 地址，转发包
-            if dst_ip in self.hosts:
-                dst_mac = self.hosts[dst_ip]
-                out_port = self.mac_to_port[dpid].get(dst_mac)
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
 
-                if out_port:
-                    actions = [parser.OFPActionOutput(out_port)]
-                    data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
-                    out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                            in_port=in_port, actions=actions, data=data)
-                    datapath.send_msg(out)
-                    return
+        actions = [parser.OFPActionOutput(out_port)]
 
-        # 如果目的 MAC 不在表中，使用 FLOOD 泛洪
-            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-            data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
-            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                    in_port=in_port, actions=actions, data=data)
-            datapath.send_msg(out)
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
 
 
-    def install_path(self, path, src_ip, dst_ip):
+    '''def install_path(self, path, src_ip, dst_ip):
         for i in range(len(path) - 1):
             datapath = self.get_datapath(path[i])
             next_dpid = path[i + 1]
@@ -108,11 +114,11 @@ class SimpleSwitch13(app_manager.RyuApp):
 
             match = parser.OFPMatch(eth_type=0x0800, ipv4_src=src_ip, ipv4_dst=dst_ip)
             actions = [parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, 1, match, actions)'''
 
     @set_ev_cls(event.EventSwitchEnter)
     def get_topology(self, ev):
-        time.sleep(3)  # 增加等待时间，确保链路信息已经准备好
+        time.sleep(1)  # 增加等待时间，确保链路信息已经准备好
 
         switches = get_switch(self, None)
         switch_list = [switch.dp.id for switch in switches]
